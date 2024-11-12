28f19b6a05167282fff2f03f36a604da3895aa21
Luzdary Mariana Canales Loyola <canalesloyolamariana@gmail.com>
2024-11-12 12:20:06 -0500

    Ultima correccion antes de la presentacion

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,19 +5,22 @@ export default function Card() {
     return (
         <article className="card">
             <figure className="card__figure">
-            {/*<div className="card_badge">¡Oferta Especial!</div>*/}
+                {/*<div className="card_badge">¡Oferta Especial!</div>*/}
                 <img className="card__image" src={pastel} alt="" loading="lazy" />
             </figure>
             <div className="card__body">
                 <h3 className="card__title">Pedazo de Pastel con Coco</h3>
                 <p className="card__text">We come with a choice of coffee for coffee lovers. With coffee taken from the best place and mixed by the owner.</p>
                 <div className='card__actions'>
-                    <p className='card__price--old'>$100.00</p>
-                    <p className='card__price'>$87.00</p>
+                    <div className='card__price'>
+                        <p className='card__price__old'>$100.00</p>
+                        <p className='card__price__new'>$87.00</p>
+
+                    </div>
                     <button className='card__btn'>Comprar</button>
                 </div>
             </div>
-          
+
         </article>
     )
 }
